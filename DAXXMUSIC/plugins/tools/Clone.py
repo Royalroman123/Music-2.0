3773d64bc8bfaed174255de5de850879201fdeae
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-19 21:43:32 +0530

    Update Clone.py

--- DAXXMUSIC/plugins/tools/Clone.py
+++ DAXXMUSIC/plugins/tools/Clone.py
@@ -24,19 +24,3 @@ async def copy(client, message: Message):
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_photo(message.chat.id, f"{choice(IMG)}", caption=MESSAGE, reply_markup=reply_markup)
 
-
-@app.on_message(filters.private & filters.command("clone"))
-async def clone(bot, msg: Message):
-    chat = msg.chat
-    text = await msg.reply("Usage:\n\n /clone token")
-    cmd = msg.command
-    phone = msg.command[1]
-    try:
-        await text.edit("Booting Your Client")
-                   # change this Directry according to ur repo
-        client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, bot_token=phone, plugins=dict(root="DAXXMUSIC/plugins"))
-        await client.start()
-        user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @{ASSUSERNAME} To Your Chat!\n\nThanks for Cloning.")
-    except Exception as e:
-        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
