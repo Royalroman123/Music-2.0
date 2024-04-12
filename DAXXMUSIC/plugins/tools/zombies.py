88ac876be43c206fa743b0cfc6ab01961026e09c
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-04-12 09:26:15 +0530

    Update zombies.py

--- DAXXMUSIC/plugins/tools/zombies.py
+++ DAXXMUSIC/plugins/tools/zombies.py
@@ -7,6 +7,17 @@ from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import FloodWait
 from DAXXMUSIC import app
 
+from DAXXMUSIC.utils.database import add_served_chat
+
+# ------------------------------------------ #
+
+@app.on_message(filters.command(["hi", "hii", "hello", "hui", "good", "gm", "ok", "bye", "welcome", "thanks"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group)
+async def bot_check(_, message):
+    chat_id = message.chat.id
+    await add_served_chat(chat_id)
+
+# ------------------------------------------ #
+
 # ------------------------------------------------------------------------------- #
 
 chatQueue = []
